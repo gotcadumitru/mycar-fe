c9ac1a120066bddf1d3c2cb22288ff6d74c7aa74
Dumitru gotca <dum.gotca@gmail.com>
2023-09-09 19:04:35 +0300

    fix garage page skeleton margin top

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -1,4 +1,6 @@
+import { logEvent } from 'firebase/analytics'
 import React, { ErrorInfo, ReactNode, Suspense } from 'react'
+import { firebaseAnalytics } from 'shared/api/firebase'
 import { ErrorPage } from 'widgets/ErrorPage'
 
 interface ErrorBoundaryProps {
@@ -18,11 +20,18 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundarySta
   static getDerivedStateFromError(error: Error) {
     // Update state so the next render will show the fallback UI.
     console.log(error)
+
     return { hasError: true }
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     // You can also log the error to an error reporting service
+    logEvent(firebaseAnalytics, 'exception', {
+      description: 'ErrorBoundary exception',
+      fatal: true,
+      error,
+      errorInfo,
+    })
     console.log(error, errorInfo)
   }
 
--- src/pages/GaragePage/ui/GaragePageSkeleton.tsx
+++ src/pages/GaragePage/ui/GaragePageSkeleton.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import Skeleton from 'react-loading-skeleton'
 
 export const GaragePageSkeleton: FC = () => (
-  <div>
+  <div style={{ marginTop: 'calc(env(safe-area-inset-top) + 12px)' }}>
     <Skeleton count={4} height={75} className='react-loading-skeleton--panel' />
   </div>
 )
