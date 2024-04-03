6e7aa421a049013a70ecad14b74d448ef96185c7
Dumitru gotca <dum.gotca@gmail.com>
2024-04-03 10:55:26 +0300

    refactor error message

--- src/pages/GaragePage/ui/GaragePage.tsx
+++ src/pages/GaragePage/ui/GaragePage.tsx
@@ -2,13 +2,12 @@ import GarageVehicleCard from 'features/garageVehicleCard'
 import { useAppSelector } from 'shared/lib/hooks/reduxHooks'
 import './garagePage.scss'
 
+const notFoundMessage = 'Nu exista vehicule adaugate, poti adauga de pe pagina principala'
 const GaragePage = () => {
   const allVehiclesOfCurrentUser = useAppSelector((state) => state.vehicle.allVehiclesOfCurrentUser)
-
   return (
     <div className='garage-page'>
-      {!allVehiclesOfCurrentUser.length &&
-        'Nihuia. nu ai masini, mergi pe pagina principala ca nu am adaugat inca un buton aici, vino dupa ce iti adaugi o masina'}
+      {!allVehiclesOfCurrentUser.length && notFoundMessage}
       {allVehiclesOfCurrentUser.map((vehicle) => (
         <GarageVehicleCard key={vehicle.uid} vehicle={vehicle} />
       ))}
