4a9c47b67cfc2933f4702f7f66f7be1fc7b3aef1
Dumitru Gotca <dum.gotca@gmail.com>
2024-05-01 15:40:14 +0100

    add panel route

--- src/shared/config/router/RoutePaths.ts
+++ src/shared/config/router/RoutePaths.ts
@@ -36,6 +36,7 @@ export const RoutePaths: Record<AppRoutes, string> = {
 
 export const RoutePathsFn = {
   getRoot: () => RoutePaths.root,
+  getPanel: () => RoutePaths.panel,
   getNewVehicle: () => RoutePaths.new_vehicle,
   getEditVehicle: (vehicleId: string) => `${RoutePaths.vehicle}/${vehicleId}`,
   getEditVehicleDocument: (vehicleId: string) => `${RoutePaths.edit_vehicle_document}/${vehicleId}`,
